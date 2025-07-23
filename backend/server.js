2acfff66788d3e05e814026f469e2745f5fe9cbf
arnab-1 <arnab.vcc@gmail.com>
2025-07-23 12:00:36 +0530

    connect with backend and frontend and implement Register component

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 dotenv.config();
 app.use(express.json());
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
 const PORT = 7000;
 
 app.use('/api/auth',authRoutes);
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,6 +1,41 @@
+import React, { use } from "react";
+import { useState } from "react";
+import API from '../utils/api'
+
+
 const Register = () => {
+
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const [isRegister,setIsRegister] = useState(false);
+
+    const handleRegister = async () => {
+        const res = await API.post('/register',{ username, password});
+
+        console.log("Registered ", res.data);
+        setUsername('');
+        setPassword('');
+        setIsRegister(true);
+    }
+
+
     return (
-        <h4> This is Register Page</h4>
+       <>
+            <h2> Register</h2>
+            <input
+            placeholder="username"
+            value={username}
+            onChange={(e) => (setUsername(e.target.value))}
+            />
+            <input
+            placeholder="password"
+            value={password}
+            onChange={(e) => (setPassword(e.target.value))}
+            />
+
+            <button type="submit" onClick={handleRegister}>Register</button>
+        
+       </>
     );
 }
 
