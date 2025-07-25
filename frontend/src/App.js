d5d1494a84db1d7d201d256dcd2d3c498b210135
arnab-1 <arnab.vcc@gmail.com>
2025-07-25 13:52:53 +0530

    Added Home component and change frontend default port

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register  from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Home  from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element = {<Home/>} />
         <Route path='/register' element = {<Register/>} />
         <Route path='/login' element = {<Login/>} />
         <Route path='/dashboard' element = {<Dashboard/>} />
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h3> This is home page</h3>
+    );
+}
+
+export default Home;
